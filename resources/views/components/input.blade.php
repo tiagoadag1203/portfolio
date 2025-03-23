6ee7813a8a1d49a7ee40c0d28c64d3b9c194589b
Tiago Augusto <tiago.adag@gmail.com>
2025-03-22 22:56:42 -0300

    ✨ feat: Adicionar valor padrão para campos de entrada e textarea para não ser necessario definir a variavel

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,9 +1,9 @@
 <div class="flex column gap-5" style="width: 100%;">
     <label for="{{ $name }}">{{ $label }}</label>
     @if($type === 'textarea')
-        <textarea class="input" name="{{ $name }}" id="{{ $id }}" {{ $required ? 'required' : '' }}>{{ $value }}</textarea>
+        <textarea class="input" name="{{ $name }}" id="{{ $id }}" {{ $required ? 'required' : '' }}>{{ $value ?? '' }}</textarea>
     @else
-        <input class="input" type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" {{ $required ? 'required' : '' }} value="{{ $value }}" />
+        <input class="input" type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" {{ $required ? 'required' : '' }} value="{{ $value ?? '' }}" />
     @endif
 </div>
 <style>
