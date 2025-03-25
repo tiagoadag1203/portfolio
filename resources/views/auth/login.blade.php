ac5e3cf268715d358be867d36d0f0cbed21c21b8
Tiago Augusto <tiago.adag@gmail.com>
2025-03-25 20:52:27 -0300

    🎨 Refactor: Refatorar rotas de autenticação.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 <main class="main flex column center-horizontal center-vertical">
     <div class="flex column center-horizontal center-vertical container-login gap-20">
         <h2>Login</h2>
-        <form class="flex column center-horizontal center-vertical gap-20 form-login" method="POST" action="/login">
+        <form class="flex column center-horizontal center-vertical gap-20 form-login" method="POST" action="{{ route('login') }}">
             @csrf
             <x-input label="E-mail:" name="email" id="email" type="email" required />
             <x-input label="Senha:" name="password" id="password" type="password" required />
