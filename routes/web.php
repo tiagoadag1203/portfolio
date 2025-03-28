6e9131ab726f784325834607bf0f974297177ca5
Tiago Augusto <tiago.adag@gmail.com>
2025-03-27 21:37:59 -0300

    🎨 Refactor: Arrumando rotas do dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,10 @@ Route::middleware('auth')->group(function () {
         return view('admin.dashboard');
     })->name('dashboard');
     Route::prefix('dashboard')->group(function () {
-        Route::resource('personal-info', PersonalInfoController::class);
+        Route::resource('personal-info', PersonalInfoController::class)->only(['index', 'update']);
         Route::resource('skills', SkillController::class)->only(['index', 'store', 'update', 'destroy']);
         Route::resource('certificates', CertificateController::class)->only(['store', 'show', 'update', 'destroy']);
-        Route::resource('personal-info', PersonalInfoController::class)->only(['index', 'update']);
-        Route::resource('skills', SkillController::class);
-        Route::resource('certificates', CertificateController::class);
-        Route::resource('experiences', ExperienceController::class);
-        Route::resource('projects', ProjectController::class)->only(['index', 'store', 'update', 'destroy']);
         Route::resource('experiences', ExperienceController::class)->only(['index', 'store', 'update', 'destroy']);
-        Route::resource('projects', ProjectController::class);
+        Route::resource('projects', ProjectController::class)->only(['index', 'store', 'update', 'destroy']);
     });
 });
